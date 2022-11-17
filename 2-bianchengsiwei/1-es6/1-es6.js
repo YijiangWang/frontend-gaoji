d5ed0884a1af86eb4cf8af024020f1003a4f9b66
yijiangwang <983148317@qq.com>
2022-11-17 19:29:49 +0800

    Set & WeakSet, Map & WeakMap

--- 2-bianchengsiwei/1-es6/1-es6.js
+++ 2-bianchengsiwei/1-es6/1-es6.js
@@ -42,4 +42,55 @@ const {name: myName, age, hobby: {ball, sport: sports}} = obj2;
 let map = new Map();
 map.set('name', 'zhangsan');
 map.set('age', 18);
-console.log(obj2.size, map.size);
+// console.log(obj2.size, map.size);
+
+// =======weakmap
+// 键名只能是对象；弱引用关系，最终会被垃圾回收机制回收，性能更好一些
+let m = new Map();
+let vm = new WeakMap();
+
+let container = {
+  key: {name:'yjw'}
+};
+
+m.set(container.key, 'Val');
+vm.set(container.key, 'VMVal')
+container.key = null;
+// console.log(container)
+// console.log(m)
+// console.log(vm);
+
+// ========Set
+let s = new Set([1,2,3,3,{a:2},{a:2}]);
+// console.log(s);
+// console.log(s.size);
+// { 1, 2, 3, { a: 2 }, { a: 2 } }
+// 5
+
+// 数组去重
+[...new Set([1,2,3,4,4,5,5,6])];
+Array.from(new Set([1,2,3,4,4,5,5,6]));
+
+// 字符串去重
+[...new Set('Hellohello')].join('');
+// console.log([...new Set('Hellohello')].join(''));
+
+// s.clear()
+// console.log(s.has(9));
+// console.log(s);
+
+// ======WeakSet
+// console.log(new WeakSet([{a:1,b:2}]))
+
+// =========Symbol
+let s1 = Symbol(1);
+let s2 = Symbol(2);
+// console.log(s1==s2);
+// false
+
+let obj5 = {
+  [Symbol(1)]: 'val',
+  name: 'yjw' 
+}
+let res = Object.getOwnPropertySymbols(obj5);
+console.log(obj5[res[0]]);
